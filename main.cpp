c111a0c9084361147297e2f8df8bb5deba7448f9
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2024-01-21 18:58:00 +0300

    Add more functionality to powershell script

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 int main(int argc, char const *argv[])
 {
     std::cout << "Hello, world!" << std::endl;
+
+    int x;
+    std::cin >> x;
     return 0;
 }
--- run.ps1
+++ run.ps1
@@ -1,8 +1,39 @@
-g++ -o main main.cpp
-$filePath = "$pwd\main.exe"
+$exeFilePath = "$pwd\main.exe"
 
-if ([System.IO.File]::Exists($filePath)) {
-    .\main.exe
+function Write-ColorOutput($ForegroundColor)
+{
+    # save the current color
+    $fc = $host.UI.RawUI.ForegroundColor
+    # set the new color
+    $host.UI.RawUI.ForegroundColor = $ForegroundColor
+
+    # output
+    if ($args) {
+        Write-Output $args
+    }
+    else {
+        $input | Write-Output
+    }
+
+    # restore the original color
+    $host.UI.RawUI.ForegroundColor = $fc
+}
+
+if ([System.IO.File]::Exists($exeFilePath)) {
+    Remove-Item -Path $exeFilePath
+}
+
+Write-ColorOutput green("Building......")
+g++ -o main main.cpp
+
+if ([System.IO.File]::Exists($exeFilePath)) {
+    $proc = Start-Process $exeFilePath -NoNewWindow -PassThru
+
+    Write-ColorOutput green("Running......`n")
+    $proc.WaitForExit()
+    Write-ColorOutput green("`nFinished.... Deleting exe file.`n")
+
+    Remove-Item -Path $exeFilePath
 } else {
-    write-host("File couldn't found!")
+    Write-ColorOutput red("File couldn't found!`n")
 }
