68288eccb164a546237e90de240681827ba2ab19
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2024-01-31 21:56:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,25 @@
 
 using namespace std;
 
+void ifElseIntro()
+{
+    cout << "Enter a number: ";
+    int num;
+    cin >> num;
+
+    if (num % 2 == 0)
+    {
+        cout << "It is even number";
+    }
+    else
+    {
+        cout << "It is odd number";
+    }
+}
+
 int main()
 {
-    char ary[] = "C++ Tutorial";
-    cout << "Value of ary is: " << ary << endl;
+    ifElseIntro();
 
     return 0;
 }
