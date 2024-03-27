9a1bbd55da167407885e1a83322270cae11dc611
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2024-03-27 12:34:51 +0300

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,10 +2,33 @@
 
 int main()
 {
-    int x;
-    x = 5;
-
-    std::cout << x;
 
     return 0;
+}
+
+void variables()
+{
+    int age = 100;
+    int year = 2024;
+    int days = 7.5;
+
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    char grade = 'A';
+    char initial = 'B';
+    char dollarSign = '$';
+
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    std::string name = "John";
+    std::string day = "Friday";
+    std::string food = "Pizza";
+    std::string address = "Test Address";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old." << '\n';
 }
