44730abaac67bf1fb1f6abe5396dde3f660e1a89
Hakan Yılmaz <76787294+hakanyilmazz@users.noreply.github.com>
2024-03-28 16:27:27 +0300

    const + namespace

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 
+namespace first
+{
+    int x = 1;
+    int y = 1;
+}
+
+namespace second
+{
+    int x = 2;
+}
+
 int main()
 {
+    using namespace first;
+
+    int x = 0;
+
+    std::cout << x << std::endl;
+    std::cout << first::x << std::endl;
+    std::cout << y << std::endl;
+    std::cout << second::x << std::endl;
 
     return 0;
+}
+
+void const_variables()
+{
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << " cm";
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
 }
 
 void variables()
